5ce72f1347a454025a3a6da0b50f57f93f2d6139
limaoyuan <android_li@sina.cn>
2016-08-27 12:00:34 +0800

    project init

---
+++ app/pages/account/account.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/account/account.html'
+})
+export class AccountPage {
+  constructor(private navCtrl: NavController) {
+
+  }
+}
